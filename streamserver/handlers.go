df322aa4d9c7652eee703ff284752b7f1acd3902
San <18814184179@163.com>
2019-10-28 19:08:40 +0800

    streaming模块

--- streamserver/handlers.go
+++ streamserver/handlers.go
@@ -6,6 +6,9 @@ package main
 import (
 	"github.com/julienschmidt/httprouter"
 	"html/template"
+	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -32,5 +35,34 @@ func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) 
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	r.Body = http.MaxBytesReader(w, r.Body, MAX_UPLOAD_SIZE)
+	if err := r.ParseMultipartForm(MAX_UPLOAD_SIZE); err != nil {
+		sendErrorResponse(w, http.StatusBadRequest, "File is too big.")
+		return
+	}
 
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal server error.")
+		return
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Printf("Read file error: %v", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal server error.")
+		return
+	}
+
+	//fn := p.ByName("vid-id")
+
+	err = ioutil.WriteFile(VIDEO_DIR+"test.mp4", data, 0666)
+	if err != nil {
+		log.Printf("Write file error: %v", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal server error")
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	io.WriteString(w, "Uploaded Successfully")
 }
