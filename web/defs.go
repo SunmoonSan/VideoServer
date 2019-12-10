73591f630ee11b4c9d12ea41854b1f19da33af01
SunmoonSan <18814184179@163.com>
2019-12-10 23:42:09 +0800

    web模块

--- web/defs.go
+++ web/defs.go
@@ -1,1 +1,12 @@
 package main
+
+type Err struct {
+	Error     string `json:"error"`
+	ErrorCode string `json:"error_code"`
+}
+
+var (
+	ErrorRequestNotRecognized   = Err{Error: "api not recognized, bad resquest", ErrorCode: "001"}
+	ErrorRequestBodyParseFailed = Err{Error: "request body is not correct", ErrorCode: "002"}
+	ErrorInternalFaults         = Err{Error: "internal service error", ErrorCode: "003"}
+)
--- web/handler.go
+++ web/handler.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 	"html/template"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -11,13 +14,63 @@ type HomePage struct {
 	Name string
 }
 
+type UserPage struct {
+	Name string
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	p := &HomePage{Name: "San"}
-	t, e := template.ParseFiles("./template/home.html")
+	cname, err1 := r.Cookie("username")
+	sid, err2 := r.Cookie("session")
+	if err1 != nil || err2 != nil {
+		p := &HomePage{Name: "San"}
+		t, e := template.ParseFiles("./templates/home.html")
+		if e != nil {
+			log.Printf("Parsing template homt.html error: %s", e)
+			return
+		}
+
+		t.Execute(w, p)
+		return
+	}
+
+	if len(cname.Value) != 0 && len(sid.Value) != 0 {
+		http.Redirect(w, r, "/userhome", http.StatusFound)
+		return
+	}
+}
+
+func userHomeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	cname, err1 := r.Cookie("username")
+	_, err2 := r.Cookie("session")
+	if err1 != nil || err2 != nil {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
+	fname := r.FormValue("username")
+
+	var p *UserPage
+	if len(cname.Value) != 0 {
+		p = &UserPage{Name: cname.Value}
+	} else if len(fname) != 0 {
+		p = &UserPage{Name: fname}
+	}
+
+	t, e := template.ParseFiles("./templates/userhome.html")
 	if e != nil {
-		log.Printf("Parsing template homt.html error: %s", e)
+		log.Print("Parsing userhome.html error: %s", e)
 		return
 	}
 	t.Execute(w, p)
-	return
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	if r.Method != http.MethodPost {
+		re, _ := json.Marshal(ErrorRequestNotRecognized)
+		io.WriteString(w, string(re))
+		return
+	}
+
+	res, _ := ioutil.ReadAll(r.Body)
+	apiBody := &ApiBody{}
 }
